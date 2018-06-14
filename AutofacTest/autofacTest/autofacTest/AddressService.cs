cfeee3404e0c1ac120cf74aa4c40c86c2e4aa045
Princebin <727269092@qq.com>
2018-06-14 09:14:29 +0800

    Autofac应用

---
+++ AutofacTest/autofacTest/autofacTest/AddressService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace autofacTest
+{
+    public class AddressService : IAddressService
+    {
+        public string GetAddress(int contactNumber)
+        {
+            string result = string.Empty;
+            if (contactNumber == 123)
+                result = "上海浦东";
+            return result;
+        }
+    }
+}
---
+++ AutofacTest/autofacTest/autofacTest/Program.cs
@@ -0,0 +1,17 @@
+﻿using Autofac;
+
+namespace autofacTest
+{
+    class Program
+    {
+        private static IContainer Container { get; set; }
+        static void Main(string[] args)
+        {
+            var builder = new ContainerBuilder();
+            builder.RegisterType<AddressService>().As<IAddressService>();
+            Container = builder.Build();
+            AddressService addressService = Container.Resolve<AddressService>();
+            addressService.GetAddress(123);
+        }
+    }
+}

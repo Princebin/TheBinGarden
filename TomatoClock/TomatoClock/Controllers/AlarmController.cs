756164ba61af488b65d53ec3a4c321a3959ac2b3
Princebin <727269092@qq.com>
2018-06-25 09:19:35 +0800

    增加闹钟功能1

---
+++ TomatoClock/TomatoClock/Controllers/AlarmController.cs
@@ -0,0 +1,34 @@
+﻿using Ay.MvcFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TomatoClock.Models;
+using TomatoClock.Views.Alarm;
+
+namespace TomatoClock.Controllers
+{
+    public class AlarmController : Controller
+    {
+        public AlarmController() : base()
+        {
+            Model = new AlarmModel();
+            Model.Groupings.Add(new Grouping { Key = 0, Text = "全部分组" });
+            Model.Groupings.Add(new Grouping { Key = 1, Text = "创建分组" });
+            Model.GroupingItem = Model.Groupings.FirstOrDefault();
+            NewAlarm = inParam =>
+            {
+                NewAlarmMethod();
+            };
+        }
+
+        private void NewAlarmMethod()
+        {
+            AlarmSeting alarmSeting = new AlarmSeting();
+            alarmSeting.ShowDialog();
+        }
+
+        public AlarmModel Model { get; set; }
+        public ActionResult NewAlarm { get; private set; }
+    }
+}
---
+++ TomatoClock/TomatoClock/Controllers/AlarmSetingController.cs
@@ -0,0 +1,29 @@
+﻿using Ay.MvcFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TomatoClock.Controllers
+{
+    public class AlarmSetingController : Controller
+    {
+        private string RoundrobinModel { get; set; }
+        public AlarmSetingController()
+        {
+            RoundrobinModel = "一次";
+            Roundrobin = inParam =>
+             {
+                 ChooseRoundrobin(inParam.ToObjectString());
+             };
+        }
+
+        private void ChooseRoundrobin(string v)
+        {
+            RoundrobinModel = v;
+            AYUI.Message.Send<string>(v, "ChooseRoundrobin");
+        }
+
+        public ActionResult Roundrobin { get; private set; }
+    }
+}

dcbaf3dc21532b3f493fed54a39bf0a5945f1d59
Princebin <727269092@qq.com>
2018-06-27 08:58:09 +0800

    设计模式

---
+++ shejimos/shejimos/Cake.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace shejimos
+{
+    public class Cake:ICloneable
+    {
+        string Name { get; set; }
+        Flur flur;
+
+        public Cake(string name)
+        {
+            Name = name;
+            flur = new Flur();
+        }
+
+        public Cake(Flur flur)
+        {
+            this.flur = (Flur)flur.Clone();
+        }
+
+        public void SetFlur(string growDate,string name)
+        {
+            flur.GrowDate = growDate;
+            flur.Name = name;
+        }
+        public void Display()
+        {
+            Console.WriteLine("Cake'Name:" + Name);
+            Console.WriteLine("Flur'Name:" + flur.Name);
+            Console.WriteLine("Flur'GrowDate:" + flur.GrowDate);
+
+        }
+
+        public object Clone()
+        {
+            Cake obj = new Cake(this.flur);
+            obj.Name = this.Name;
+            return obj;
+        }
+    }
+    public class Flur : ICloneable
+    {
+        public string GrowDate { get; set; }
+        public string Name { get; set; }
+
+        public object Clone()
+        {
+            return (object)this.MemberwiseClone();
+        }
+    }
+}

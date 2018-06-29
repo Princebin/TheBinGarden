03121507438cc56fa74f983559f9dd17ea4be0b1
Princebin <727269092@qq.com>
2018-06-29 08:50:16 +0800

    修改3

---
+++ DesignPattern/DesignPattern/Drink.cs
@@ -0,0 +1,83 @@
+﻿using System;
+
+namespace DesignPattern
+{
+    /// <summary>
+    /// 适用场景：
+    //扩展一个类的功能。
+    //动态增加功能，动态撤销。
+
+    //优点：
+    //装饰类和被装饰类可以独立发展，不会相互耦合
+    //动态的将责任附加到对象身上。
+    //缺点：
+    //多层装饰比较复杂。
+    /// </summary>
+    public class Drink
+    {
+        public virtual float Cost()
+        {
+            return 0;
+        }
+        public virtual String GetDescription()
+        {
+            return null;
+        }
+    }
+    public class Coffee : Drink
+    {
+        public override float Cost()
+        {
+            return 10;
+        }
+        public override String GetDescription()
+        {
+            return "coffee";
+        }
+    }
+    public class CondimentDecorator : Drink
+    {
+        protected Drink decoratorDrink;
+
+        public CondimentDecorator(Drink decoratorDrink)
+        {
+            this.decoratorDrink = decoratorDrink;
+        }
+        public override float Cost()
+        {
+            return decoratorDrink.Cost();
+        }
+        public override String GetDescription()
+        {
+            return decoratorDrink.GetDescription();
+        }
+    }
+    public class Milk : CondimentDecorator
+    {
+        public Milk(Drink decoratorDrink) : base(decoratorDrink)
+        {
+        }
+        public override float Cost()
+        {
+            return base.Cost() + 2;
+        }
+        public override string GetDescription()
+        {
+            return base.GetDescription() + " milk";
+        }
+    }
+    public class Sugar : CondimentDecorator
+    {
+        public Sugar(Drink decoratorDrink) : base(decoratorDrink)
+        {
+        }
+        public override float Cost()
+        {
+            return base.Cost() + 1;
+        }
+        public override string GetDescription()
+        {
+            return base.GetDescription() + " sugar";
+        }
+    }
+}
---
+++ DesignPattern/DesignPattern/MakeCoffee.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/// <summary>
+/// 模板方法模式
+/// </summary>
+namespace DesignPattern
+{
+    public abstract class MakeCoffee
+    {
+        private string _beanName;
+        private string _temperature;
+
+        public MakeCoffee(string beanName, string temperature)
+        {
+            _beanName = beanName;
+            _temperature = temperature;
+        }
+
+        public abstract void GrindBean(string BeanName);
+        public abstract void Steaming(string Temperature);
+        public void TemplateMethod()
+        {
+            Console.WriteLine("温杯");
+            GrindBean(_beanName);
+            Console.WriteLine("温壶");
+            Steaming(_temperature);
+            Console.WriteLine("冲泡");
+            Console.WriteLine("咖啡泡好了！");
+        }
+    }
+    public class Latte : MakeCoffee
+    {
+        public Latte(string beanName, string temperature) : base(beanName, temperature)
+        {
+        }
+
+        public override void GrindBean(string BeanName)
+        {
+            Console.WriteLine(BeanName);
+        }
+
+        public override void Steaming(string Temperature)
+        {
+            Console.WriteLine("温度" + Temperature);
+        }
+    }
+    public class Lanshan : MakeCoffee
+    {
+        public Lanshan(string beanName, string temperature) : base(beanName, temperature)
+        {
+        }
+
+        public override void GrindBean(string BeanName)
+        {
+            Console.WriteLine(BeanName);
+        }
+
+        public override void Steaming(string Temperature)
+        {
+            Console.WriteLine("温度" + Temperature);
+        }
+    }
+}
